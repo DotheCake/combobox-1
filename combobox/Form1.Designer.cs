dae45553d68d22d2e8d18d16e648fc742145842f
mariana.fmollo <mariana.fmollo@AME0613387W10-1>
2024-04-17 21:37:23 -0300

    Adicionar arquivos de projeto.

---
+++ combobox/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace combobox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEscolha = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cmbEscolha = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEscolha
+            // 
+            this.btnEscolha.Location = new System.Drawing.Point(392, 72);
+            this.btnEscolha.Name = "btnEscolha";
+            this.btnEscolha.Size = new System.Drawing.Size(280, 35);
+            this.btnEscolha.TabIndex = 0;
+            this.btnEscolha.Text = "Escolha um numero e clique aqui!";
+            this.btnEscolha.UseVisualStyleBackColor = true;
+            this.btnEscolha.Click += new System.EventHandler(this.btnEscolha_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SAIR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cmbEscolha
+            // 
+            this.cmbEscolha.FormattingEnabled = true;
+            this.cmbEscolha.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.cmbEscolha.Location = new System.Drawing.Point(265, 76);
+            this.cmbEscolha.Name = "cmbEscolha";
+            this.cmbEscolha.Size = new System.Drawing.Size(103, 28);
+            this.cmbEscolha.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Escolha um numero de 1 a 12:";
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.Location = new System.Drawing.Point(65, 118);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(0, 29);
+            this.lblMensagem.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Digite seu nome:";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(159, 34);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(513, 26);
+            this.textBoxNome.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 265);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbEscolha);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnEscolha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEscolha;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cmbEscolha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNome;
+    }
+}
+
---
+++ combobox/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace combobox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEscolha_Click(object sender, EventArgs e)
+        {
+            string nome = textBoxNome.Text;
+            switch (Convert.ToInt32(cmbEscolha.SelectedItem))
+            {
+                case 1:
+                    lblMensagem.Text = "Olá, " + nome + " você nasceu em Janeiro";
+                    break;
+
+                case 2:
+                    lblMensagem.Text = "Olá, " + nome + " você nasceu em Fevereiro";
+                    break;
+
+                case 3:
+                    lblMensagem.Text = "Olá, " + nome + " você nasceu em Março";
+                    break;
+
+                case 4:
+                    lblMensagem.Text = "Olá, " + nome + " você nasceu em Abril";
+                    break;
+
+                case 5:
+                    lblMensagem.Text = "Olá, "  + nome + " você nasceu em Maio";
+                    break;
+
+                case 6:
+                    lblMensagem.Text = "Olá, " + nome + " você nasceu em Junho";
+                    break;
+
+                case 7:
+                    lblMensagem.Text = "Olá ," + nome + " você nasceu em Julho";
+                    break;
+
+                case 8:
+                    lblMensagem.Text = "Olá, " + nome + " você nasceu em Agosto";
+                    break;
+
+                case 9:
+                    lblMensagem.Text = "Olá, " + nome + " você nasceu em Setembro";
+                    break;
+
+                case 10:
+                    lblMensagem.Text = "Olá, " + nome + " você nasceu em Outubro";
+                    break;
+
+                case 11:
+                    lblMensagem.Text = "Olá, " + nome + " você nasceu em Novembro";
+                    break;
+
+                case 12:
+                    lblMensagem.Text = "Olá, " + nome + " você nasceu me Dezembro";
+                    break;
+ 
+                default:
+                    lblMensagem.Text = "Por favor escolha uma opção!";
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
